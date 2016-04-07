75dfb30d431b22c9ad7b68ecbed809f73965f24e
Aad Versteden <madnificent@gmail.com>
2016-04-07 19:16:33 +0200

    Add topics and order to pages
    The topic is the thing that should appear on the button.  The order is
    the place where it should appear in the menu.  Low number, is early in
    the menu.

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -6,5 +6,7 @@
   :class (s-url "http://mu.semte.ch/vocabulary/cms/Page")
   :resource-base (s-url "http://mu.semte.ch/cms/resources/pages/")
   :properties `((:title :string ,(s-prefix "dcterms:title"))
+                (:topic :string ,(s-prefix "cms:pageName"))
+                (:order :number ,(s-prefix "cms:menuOrder"))
                 (:content :string ,(s-prefix "cms:pageContent")))
   :on-path "pages")
